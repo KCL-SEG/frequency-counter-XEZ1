6aa9ebd227373aa7b781eeacd8a3d01776b5aa2b
XEZ1 <xxxaclockworkorangegmail.com>
2022-10-02 04:15:09 +0100

    Solve the frequencies problem

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,28 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+
+    #count = 0
+    #for i in items:
+    #    for j in items:
+    #        if i == j:
+    #            count += 1
+    #    frequencies[i] = count
+    #    for k in range(count):
+    #        items.remove(i)
+    #   count = 0
+
+    for i in range(len(items)):
+        if type(items[i]) != str:
+            items[i] = str(items[i])
+
+    for i in range(len(items)):
+        frequencies[items[i]] = items.count(items[i])
+        for j in range(items.count(i)):
+            items.remove(i)
+
+
+
     return frequencies
+
+print(frequencies(['a', 'a', 'b', 'b', 'b', 'c']))
